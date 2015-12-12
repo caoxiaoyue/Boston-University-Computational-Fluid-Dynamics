61e5fb98ac00ffee223bfa43e00dd90f7b556034
Mark R. Lytell <lytemar@yahoo.com>
2015-12-11 20:30:18 -0600

    Fixed bugs in Steps 1-3, and completed step 4.

--- Assignment Steps 1 to 4/Step4.py
+++ Assignment Steps 1 to 4/Step4.py
@@ -1,10 +1,10 @@
 import numpy as np
-# from initial_conditions import *
+from boundary_conditions import *
 import matplotlib.pyplot as plt
 
 # Initial problem parameters
 xDomain = (0.0,2.0*np.pi)     # x domain
-nx = 20     # number of x-grid points
+nx = 70     # number of x-grid points
 nt = 50     # number of time steps
 dt = 0.01   # time step size
 nu = 0.1    # diffusion coefficient
@@ -16,7 +16,7 @@ u = np.zeros((nx, nt))
 # Create an array of x values based on the discretization
 x = np.arange(xDomain[0], xDomain[1]+dx, dx)
 
-# Create the initial conditions
+# Create the initial conditions.  The initial boundary conitions are taken care of
 def phi(x,nu):
 	return np.exp(-x**2/(4*nu)) + np.exp(-(x-2*np.pi)**2/(4*nu))
 
@@ -24,15 +24,15 @@ def dphi_dx(x,nu):
 	return -x/(2*nu)*np.exp(-x**2/(4*nu)) - (2*x-4*np.pi)/(4*nu)*np.exp(-(x-2*np.pi)**2/(4*nu))
 
 u[:,0] = -2*nu*dphi_dx(x,nu)/phi(x,nu) + 4
+bc = (u[0,0], u[nx-1,0])
 
 # March along time steps
-for n in range(1,nt):
+for n in range(nt-1):
+    u[0,n+1], u[nx-1,n+1] = set_boundary_conditions(bc)
     for i in range(1,nx-1):
-        u[i,n] = u[i,n-1] - u[i,n-1]*dt/dx*(u[i,n-1] - u[i-1,n-1]) + nu*dt/(dx**2)*(u[i+1,n-1] - 2.0*u[i,n-1] + u[i-1,n-1])
+        u[i, n+1] = u[i,n] - u[i,n]*dt/dx*(u[i,n] - u[i-1,n]) + nu*dt/(dx**2)*(u[i+1,n] - 2.0*u[i,n] + u[i-1,n])
 
 # Plot the velocities at the end of the computation
-
-
 fig = plt.figure()
 ax = plt.subplot(111)
 plt.ylabel('velocity')
