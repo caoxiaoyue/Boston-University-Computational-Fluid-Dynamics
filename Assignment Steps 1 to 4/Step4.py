9470c5bafd6794acf873da051058ed1601798c26
Mark R. Lytell <lytemar@yahoo.com>
2015-12-13 16:53:21 -0600

    Adjusted step 4 to use periodic boundary conditions.

--- Assignment Steps 1 to 4/Step4.py
+++ Assignment Steps 1 to 4/Step4.py
@@ -16,21 +16,48 @@ u = np.zeros((nx, nt))
 # Create an array of x values based on the discretization
 x = np.arange(xDomain[0], xDomain[1]+dx, dx)
 
+# Create auxiliary function for periodic boundary conditions
+ip1 = np.zeros(nx)
+im1 = np.zeros(nx)
+for i in range(nx):
+    ip1[i] = i+1
+    im1[i] = i-1
+ip1[nx-1] = 1
+im1[0] = nx-1
+
+#print ip1
+#print im1
+
 # Create the initial conditions.  The initial boundary conitions are taken care of
-def phi(x,nu):
+def phi(x):
 	return np.exp(-x**2/(4*nu)) + np.exp(-(x-2*np.pi)**2/(4*nu))
 
-def dphi_dx(x,nu):
+def dphi_dx(x):
 	return -x/(2*nu)*np.exp(-x**2/(4*nu)) - (2*x-4*np.pi)/(4*nu)*np.exp(-(x-2*np.pi)**2/(4*nu))
 
-u[:,0] = -2*nu*dphi_dx(x,nu)/phi(x,nu) + 4
+u[:,0] = -2*nu*dphi_dx(x)/phi(x) + 4
 bc = (u[0,0], u[nx-1,0])
+u[0,:] = u[0,0]
+u[nx-1,:] = u[0,:]
 
 # March along time steps
 for n in range(nt-1):
-    u[0,n+1], u[nx-1,n+1] = set_boundary_conditions(bc)
+    un = u[:,n]
+    #u[0,n+1], u[nx-1,n+1] = set_boundary_conditions(bc)
     for i in range(1,nx-1):
-        u[i, n+1] = u[i,n] - u[i,n]*dt/dx*(u[i,n] - u[i-1,n]) + nu*dt/(dx**2)*(u[i+1,n] - 2.0*u[i,n] + u[i-1,n])
+        u[i, n+1] = un[i] - un[i]*dt/dx*(un[i] - un[im1[i]]) +\
+                    nu*dt/(dx**2)*(un[ip1[i]] - 2.0*un[i] + un[im1[i]])
+
+# Analytical solution
+def phiAnalytical(x,t):
+    return np.exp(-(x-4*t)**2/(4*nu*(t+1))) + np.exp(-(x-4*t-2*np.pi)**2/(4*nu*(t+1)))
+
+def dphi_dx_Analytical(x,t):
+    return -1.0/(2*nu*(t+1))*((x-4*t)*np.exp(-(x-4*t)**2/(4*nu*(t+1))) +
+                              (x-4*t-2*np.pi)*np.exp(-(x-4*t-2*np.pi)**2/(4*nu*(t+1))))
+
+def uAnalytical(x,t):
+    return -2*nu*dphi_dx_Analytical(x,t)/phiAnalytical(x,t) + 4.0
 
 # Plot the velocities at the end of the computation
 fig = plt.figure()
@@ -38,8 +65,11 @@ ax = plt.subplot(111)
 plt.ylabel('velocity')
 
 for it in range(0,nt,5):
-    t = it * dt
-    ax.plot(x, u[:,it], label="t="+str(t))
+    time = it * dt
+    ax.plot(x, u[:,it], label="t="+str(time))
+
+# Plot the analytical solution
+ax.plot(x,uAnalytical(x, nt*dt), label="Analytical")
 
 # Shrink current axis' height by 10% on the bottom
 box = ax.get_position()
